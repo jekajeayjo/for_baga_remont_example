9cce19187278e27f7d609f76f37f1e499d5b475d
elayjo <sochok2024@gmail.com>
2020-04-22 20:50:24 +0200

    first commit

--- src/main/resources/static/components/main.vue
+++ src/main/resources/static/components/main.vue
@@ -6,29 +6,37 @@
                 <h1 STYLE="color: #17a2b8"> БЫСРЫЙ И КАЧЕСТВЕННЫЙ РЕМОНТ В ВАШЕМ ДОМЕ</h1>
                 <p style="color: #f8f9fa">Создайте идеальное пространство</p>
             </div>
-            <div class="col-6">
-                <img class="d-block w-100 " src="/pictures/1.jpg"/>
-            </div>
+
         </div>
     </div>
 </template>
 <script>
 
 
+
     export default {
 
 
-        components: {
-
-        },
         data() {
             return {
-
+                data: [
+                    '<div class="example-slide">Slide 1</div>',
+                    '<div class="example-slide">Slide 2</div>',
+                    '<div class="example-slide">Slide 3</div>',
+                ],
             }
         }
     }
 </script>
 <style>
 
-
+    .example-slide {
+        align-items: center;
+        background-color: #666;
+        color: #999;
+        display: flex;
+        font-size: 1.5rem;
+        justify-content: center;
+        min-height: 10rem;
+    }
 </style>
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,6 +3,7 @@ import VueResource from 'vue-resource'
 import App from '../pages/App.vue'
 import router from "../router/router";
 import map from '../pictures/1.jpg'
+
 Vue.use(VueResource),
 Vue.use(map)
 new Vue({
